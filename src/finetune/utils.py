0f08a19a1cd4157232df5cf955f9f42ae74941b0
chandanreddy10 <chandanreddy10@outlook.com>
2026-03-10 16:43:27 +0000

    update utils

--- src/finetune/utils.py
+++ src/finetune/utils.py
@@ -9,11 +9,9 @@ PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 def split_dataframe(df, output_dir, n_splits=3):
 
-    # create path relative to project root
     output_dir = PROJECT_ROOT / output_dir
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    # split dataframe
     splits = np.array_split(df, n_splits)
 
     for i, split_df in enumerate(splits):
